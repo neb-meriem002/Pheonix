963ec9653eddd0baff9d6f8f7b096e4a714b6363
SarahBenm <benmessai.sarah@gmail.com>
2024-05-25 17:43:27 +0200

    integration du login:   add_task.php

--- add_task.php
+++ add_task.php
@@ -3,7 +3,7 @@
         $errors = "";
 
         // connect to database
-        $db = mysqli_connect("localhost", "root", "", "todo");
+        $db = mysqli_connect("localhost", "root", "", "todo_list");
 
         // insert a quote if submit button is clicked
         if (isset($_POST['submit'])) {
