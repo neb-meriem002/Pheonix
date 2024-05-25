c0c4397494b1ad79e0fc341b93a55ba05dcfc9fc
neb-meriem002 <meryneb002@gmail.com>
2024-05-25 12:48:27 +0100

    interface changes 2

---
+++ script.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("sideNav").style.width = "250px";
+    document.body.style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("sideNav").style.width = "0";
+    document.body.style.marginLeft = "0";
+}
